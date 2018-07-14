d5bc684b533b8c22636c94a466d35e0166e5fc9d
Zach Freedman <zfreedman92@gmail.com>
2018-07-14 19:45:52 -0400

    feat: Began adding some simple styling to app.

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,11 @@ import React from "react";
 
 import MathDisplay from "components/mathDisplay";
 import MathInput from "components/mathInput";
+require("../index.css")
 
 export default () => {
   return (
-    <div>
+    <div className="app">
       <h1>Quick Maffs</h1>
       <MathDisplay />
       <MathInput />
