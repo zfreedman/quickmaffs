07930f2483db3c26a6910554256152d21a9d6cf1
Zach Freedman <zfreedman92@gmail.com>
2018-07-15 22:29:44 -0400

    feat: Added functionality to compute a random expression
    This function is being used to prefill the state with an initial expression based on <MathDisplay />'s state parameters

--- src/components/mathDisplay.js
+++ src/components/mathDisplay.js
@@ -1,11 +1,57 @@
 import React from "react";
 
+const OP_ADD = "+";
+const OP_SUB = "-";
+const OP_MUL = "*";
+const OP_DIV = "/";
+
 export default class MathDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expression: "",
+      opList: [OP_ADD],
+      maxNumber: 5
+    };
+
+    this.makeExpression = this.getExpression.bind(this);
+  }
+
   render() {
     return (
       <div>
-        2 + 2
+        {this.state.expression}
       </div>
+    );
+  }
+
+  componentDidMount() {
+    this.setState({
+      expression: this.getExpression(
+        this.state.opList, this.state.maxNumber
+      )
+    });
+  }
+
+  getExpression = (opList, maxNumber) => {
+    // operator
+    const op_index = this.getRandomInt(opList.length);
+    const op = this.state.opList[op_index];
+
+    // operands
+    const left = this.getRandomInt(maxNumber + 1);
+    const right = this.getRandomInt(maxNumber + 1);
+    const expression = `${left} ${op} ${right}`;
+
+    return expression;
+  }
+
+  getRandomInt(max) {
+    return Math.floor(
+      Math.random() * Math.floor(
+        max
+      )
     );
   }
 }
