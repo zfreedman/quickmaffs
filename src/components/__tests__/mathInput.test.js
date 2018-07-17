3315c692e587aa937025f8cfd86dbc1f3edb49e2
Zach Freedman <zfreedman92@gmail.com>
2018-07-17 15:32:49 -0400

    feat: Added input edit tests for <MathInput />

--- src/components/__tests__/mathInput.test.js
+++ src/components/__tests__/mathInput.test.js
@@ -2,8 +2,38 @@ import React from "react";
 import { mount } from "enzyme";
 import MathInput from "components/mathInput";
 
-it("has an input field", () => {
-  const wrapped = mount(<MathInput />);
+let wrapped;
+beforeEach(() => {
+  wrapped = mount(<MathInput />);
+});
 
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it("has an input field", () => {
   expect(wrapped.find("input").length).toEqual(1);
+});
+
+describe("input edit tests", () => {
+  const input = 732;
+  beforeEach(() => {
+    wrapped.find("input").simulate("change", {
+      target: {value: input}
+    });
+    wrapped.update();
+  });
+
+  it("has an input field users can enter numbers into", () => {
+    expect(wrapped.find("input").prop("value")).toEqual(input);
+  });
+
+  it("has an input field that clears out when users press enter", () => {
+    wrapped.find("input").simulate("keydown", {
+      keyCode: 13
+    });
+    wrapped.update();
+
+    expect(wrapped.find("input").prop("value")).toEqual("");
+  });
 });
