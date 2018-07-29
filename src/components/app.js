de316995fba6cdc63ddce1f0d2887bb6d90d2f0a
Zach Freedman <zfreedman92@gmail.com>
2018-07-29 00:24:01 -0400

    refactor: Refactored <App /> to house <MathDisplay /> old state
    <MathDisplay /> should just be repsonsible for displaying an expression, not creating it. Will now be refactoring <App />.{opList, operations, operands} into Redux state

--- src/components/app.js
+++ src/components/app.js
@@ -5,13 +5,63 @@ import MathDisplay from "components/mathDisplay";
 import MathInput from "components/mathInput";
 require("../index.css")
 
-export default () => {
-  return (
-    <div className="app">
+const OP_ADD = "+";
+const OP_SUB = "-";
+const OP_MUL = "*";
+const OP_DIV = "/";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      maxNumber: 5,
+      opList: [OP_ADD],
+      operands: [],
+      operations: [],
+    };
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="app">
       <h1>Quick Maffs</h1>
-      <MathDisplay />
+      <MathDisplay
+      operands={this.state.operands}
+        operations={this.state.operations}
+      />
       <MathInput />
       <AnswerList />
-    </div>
-  );
+      </div>
+    );
+  }
+
+  componentWillMount() {
+    this.setState(
+      this.getMath(this.state.opList, this.state.maxNumber)
+    );
+  }
+
+  getMath = (opList, maxNumber) => {
+    const op_index = this.getRandomInt(opList.length);
+    const op = opList[op_index];
+
+    const left = this.getRandomInt(maxNumber + 1);
+    const right = this.getRandomInt(maxNumber + 1);
+    return {
+      operands: [left, right],
+      operations: [op],
+    };
+  };
+
+  getRandomInt(maxNumber) {
+    return Math.floor(
+      Math.random() * Math.floor(
+        maxNumber
+      )
+    );
+  }
 };
+
+export default App;
--- src/components/mathDisplay.js
+++ src/components/mathDisplay.js
@@ -6,52 +6,23 @@ const OP_MUL = "*";
 const OP_DIV = "/";
 
 export default class MathDisplay extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      expression: "",
-      opList: [OP_ADD],
-      maxNumber: 5
-    };
-
-    this.makeExpression = this.getExpression.bind(this);
-  }
 
   render() {
     return (
       <div>
-        {this.state.expression}
+        {this.renderMathFromProps()}
       </div>
     );
   }
 
-  componentDidMount() {
-    this.setState({
-      expression: this.getExpression(
-        this.state.opList, this.state.maxNumber
-      )
-    });
-  }
-
-  getExpression = (opList, maxNumber) => {
-    // operator
-    const op_index = this.getRandomInt(opList.length);
-    const op = this.state.opList[op_index];
-
-    // operands
-    const left = this.getRandomInt(maxNumber + 1);
-    const right = this.getRandomInt(maxNumber + 1);
-    const expression = `${left} ${op} ${right}`;
-
+  renderMathFromProps() {
+    let operations = this.props.operations;
+    let operands = this.props.operands;
+    let expression = `${operands[0]} `;
+    for (let i = 1; i < operands.length; ++i) {
+      expression += `${operations[i - 1]} ${operands[i]}`;
+      expression += i < operations.length - 1 ? " " : "";
+    }
     return expression;
-  }
-
-  getRandomInt(max) {
-    return Math.floor(
-      Math.random() * Math.floor(
-        max
-      )
-    );
   }
 }
